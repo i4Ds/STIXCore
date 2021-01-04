2f783cf17a77ca8280342bf9d1d804506f20dfdb
Nicky Hochmuth <nicky.thecrag@gmail.com>
2021-01-04 15:21:43 +0100

    import fixes?

--- stixcore/tmtc/tm/__init__.py
+++ stixcore/tmtc/tm/__init__.py
@@ -1,12 +1,12 @@
 """TM packets for STIX."""
 
-import stixcore.tmtc.tm.tm_1 as tm_1
-import stixcore.tmtc.tm.tm_3 as tm_3
-import stixcore.tmtc.tm.tm_5 as tm_5
-import stixcore.tmtc.tm.tm_6 as tm_6
-import stixcore.tmtc.tm.tm_17 as tm_17
-import stixcore.tmtc.tm.tm_21 as tm_21
-import stixcore.tmtc.tm.tm_236 as tm_236
-import stixcore.tmtc.tm.tm_237 as tm_237
-import stixcore.tmtc.tm.tm_238 as tm_238
-import stixcore.tmtc.tm.tm_239 as tm_239
+from stixcore.tmtc.tm import tm_1 as tm_1
+from stixcore.tmtc.tm import tm_3 as tm_3
+from stixcore.tmtc.tm import tm_5 as tm_5
+from stixcore.tmtc.tm import tm_6 as tm_6
+from stixcore.tmtc.tm import tm_17 as tm_17
+from stixcore.tmtc.tm import tm_21 as tm_21
+from stixcore.tmtc.tm import tm_236 as tm_236
+from stixcore.tmtc.tm import tm_237 as tm_237
+from stixcore.tmtc.tm import tm_238 as tm_238
+from stixcore.tmtc.tm import tm_239 as tm_239

3b2ac1b4dd8d3f18c5370c6d86884fff8d8b7b2e
Shane Maloney <shane.maloney@tcd.ie>
2021-01-15 10:36:37 +0000

    Add a working `Packet` to tmtc module namespace

--- stixcore/tmtc/tm/__init__.py
+++ stixcore/tmtc/tm/__init__.py
@@ -1,16 +1,15 @@
 """TM packets for STIX."""
 
-from stixcore.tmtc.tm import tm_1 as tm_1
-from stixcore.tmtc.tm import tm_3 as tm_3
-from stixcore.tmtc.tm import tm_5 as tm_5
-from stixcore.tmtc.tm import tm_6 as tm_6
-from stixcore.tmtc.tm import tm_17 as tm_17
-from stixcore.tmtc.tm import tm_21 as tm_21
-from stixcore.tmtc.tm import tm_22 as tm_22
-from stixcore.tmtc.tm import tm_236 as tm_236
-from stixcore.tmtc.tm import tm_237 as tm_237
-from stixcore.tmtc.tm import tm_238 as tm_238
-from stixcore.tmtc.tm import tm_239 as tm_239
-
-__all__ = ['tm_1', 'tm_3', 'tm_5', 'tm_6', 'tm_17', 'tm_21', 'tm_22', 'tm_236', 'tm_237', 'tm_238',
-           'tm_239']
+from stixcore.tmtc.tm import (
+    tm_1,
+    tm_3,
+    tm_5,
+    tm_6,
+    tm_17,
+    tm_21,
+    tm_22,
+    tm_236,
+    tm_237,
+    tm_238,
+    tm_239,
+)

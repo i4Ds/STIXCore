37f8df2b7898791fbcc692f86226239b8de3fc2c
Nicky Hochmuth <nicky.thecrag@gmail.com>
2021-02-26 19:37:55 +0100

    doc config

--- docs/conf.py
+++ docs/conf.py
@@ -83,6 +83,15 @@ intersphinx_mapping = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
+# Workaround to install and execute git-lfs on Read the Docs
+import os
+if not os.path.exists('./git-lfs'):
+    os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.1/git-lfs-linux-amd64-v2.7.1.tar.gz')
+    os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz')
+    os.system('./git-lfs install')  # make lfs available in current repository
+    os.system('./git-lfs fetch')  # download content from remote
+    os.system('./git-lfs checkout')  # make local files to have the real content on them
+
 try:
     from sunpy_sphinx_theme.conf import *
 except ImportError:

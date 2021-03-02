bdec82f7a85fd104ac8cf2278164664031eb7422
Nicky Hochmuth <nicky.thecrag@gmail.com>
2021-03-02 12:39:40 +0100

    test for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,7 @@ import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd and not os.path.exists('./git-lfs'):
     os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.13.2/git-lfs-linux-amd64-v2.13.2.tar.gz')
-    os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz')
+    os.system('tar xvfz git-lfs-linux-amd64-v2.13.2.tar.gz')
     os.system('./git-lfs install')  # make lfs available in current repository
     os.system('./git-lfs fetch')  # download content from remote
     os.system('./git-lfs checkout')  # make local files to have the real content on them

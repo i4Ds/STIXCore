a80c000e20e607aaf46d26ba22132fac05315293
Nicky Hochmuth <nicky.thecrag@gmail.com>
2021-04-06 14:41:42 +0200

    before rebase

--- stixcore/processing/engineering.py
+++ stixcore/processing/engineering.py
@@ -1,7 +1,11 @@
 """Processing module for converting raw to engineering values."""
 import re
 
-from stixcore.tmtc.parameter import EngineeringParameter
+import numpy as np
+
+import astropy.units as u
+
+from stixcore.tmtc.parameter import EngineeringParameter, Parameter
 from stixcore.util.logging import get_logger
 
 __all__ = ['EngineeringParameter', 'raw_to_engineering']
@@ -29,9 +33,9 @@ def apply_raw_to_engineering(raw, args):
     en = None
     if param.PCF_CATEG == 'S':
         en = idb.textual_interpret(param.PCF_CURTX, raw.name)
-        if en is None:
-            logger.error(f'Missing textual calibration info for: {param.PCF_NAME} / \
-                            {param.PCF_CURTX} value={raw}')
+        # if en is None:
+        #   logger.error(f'Missing textual calibration info for: {param.PCF_NAME} / \
+        #               {param.PCF_CURTX} value={raw}')
     elif param.PCF_CATEG == 'N':
         prefix = re.split(r'\d+', param.PCF_CURTX)[0]
         if prefix == 'CIXP':
@@ -69,13 +73,78 @@ def raw_to_engineering(packet):
     `int`
         How many times the raw to engineering algorithm was called.
     """
-    calib_parameters = packet.get_calibration_params()
+    idb, calib_parameters = packet.get_calibration_params()
 
     if not calib_parameters:
         return 0
 
-    idb = packet.get_idb()
     c = 0
     for param in calib_parameters:
         c += packet.data.apply(param.PCF_NAME, apply_raw_to_engineering, (param, idb))
     return c
+
+
+def raw_to_engineering_product(product, idbm):
+    """Apply parameter raw to engineering conversion for the entire product.
+
+    Parameters
+    ----------
+    product : `BaseProduct`
+        The TM product as level 0
+
+    Returns
+    -------
+    `int`
+        How many columns where calibrated.
+    """
+    col_n = 0
+    for col in product.data.colnames:
+        if (not (hasattr(product.data[col], "meta")
+                 and "PCF_CURTX" in product.data[col].meta
+                 and product.data[col].meta["PCF_CURTX"] is not None
+                 and product.data[col].meta["NIXS"] is not None
+                 and hasattr(product, "idb")
+                 )):
+            continue
+        col_n += 1
+        c = 0
+        for idbversion, (starttime, endtime) in product.idb.items():
+            starttime = starttime * u.s
+            endtime = endtime * u.s
+
+            print(idbversion, starttime, endtime)
+            idb = idbm.get_idb(idbversion)
+            idb_time_period = np.where((starttime <= product.data['time']) &
+                                       (product.data['time'] < endtime))[0]
+            if len(idb_time_period) < 1:
+                continue
+            c += len(idb_time_period)
+            calib_param = idb.get_params_for_calibration(
+                                    product.service_type,
+                                    product.service_subtype,
+                                    (product.ssid if hasattr(product, "ssid") else None),
+                                    product.data[col].meta["NIXS"],
+                                    product.data[col].meta["PCF_CURTX"])[0]
+
+            raw = Parameter(product.data[col].meta["NIXS"],
+                            product.data[idb_time_period][col], None)
+
+            eng = apply_raw_to_engineering(raw, (calib_param, idb))
+
+            # cast the type of the column if needed
+            if product.data[col].dtype != eng.engineering.dtype:
+                product.data[col] = product.data[col].astype(eng.engineering.dtype)
+
+            # set the unit if needed
+            if product.data[col].unit != eng.engineering.unit:
+                meta = product.data[col].meta
+                product.data[col].unit = eng.engineering.unit
+                # restore the meta info
+                setattr(product.data[col], "meta", meta)
+
+            # override the data
+            product.data[col][idb_time_period] = eng.engineering
+
+        assert c == len(product.data)
+
+    return col_n
--- stixcore/products/tests/test_housekeeping.py
+++ stixcore/products/tests/test_housekeeping.py
@@ -1,15 +1,30 @@
 import re
+from time import perf_counter
 from unittest.mock import patch
 
 import pytest
 
 from stixcore.data.test import test_data
+from stixcore.idb.manager import IDBManager
+from stixcore.io.soc.manager import SOCPacketFile
+from stixcore.processing.engineering import raw_to_engineering_product
 from stixcore.products.level0.housekeeping import MaxiReport, MiniReport
+from stixcore.products.levelb.binary import LevelB
 
 testpackets = [(test_data.tmtc.TM_3_25_1, MiniReport, 'mini',
                 '0660010031:51423', '0660010031:51423', 1),
                (test_data.tmtc.TM_3_25_2, MaxiReport, 'maxi',
                 '0660258881:33104', '0660258881:33104', 1)]
+
+
+@pytest.fixture
+def idbm():
+    return IDBManager(test_data.idb.DIR)
+
+
+@pytest.fixture
+def soc_hk():
+    return test_data.io.HK_MAXI
 
 
 @patch('stixcore.products.levelb.binary.LevelB')
@@ -31,17 +46,38 @@ def test_housekeeping(levelb, packets):
 
 
 @patch('stixcore.products.levelb.binary.LevelB')
-@pytest.mark.parametrize('packets', testpackets, ids=[f[0].stem for f in testpackets])
-def test_calibration(levelb, packets):
-    hex_file, cl, name, beg, end, size = packets
-    with hex_file.open('r') as file:
+def test_calibration_hk(levelb, idbm):
+
+    with test_data.tmtc.TM_3_25_2.open('r') as file:
         hex = file.readlines()
 
     levelb.data.__getitem__.return_value = [re.sub(r"\s+", "", h) for h in hex]
 
-    hk = cl.from_levelb(levelb)
-    setattr(hk, "idb", {"2.26.34": (6.60258882e+08, 6.60258882e+08)})
+    hk = MaxiReport.from_levelb(levelb)
+
+    setattr(hk, "idb", {"2.26.34": (660258881.0, 660258882.0)})
     setattr(hk.data['hk_dpu_pcb_t'], "meta", {"NIXS": "NIXD0025", "PCF_CURTX": "CIXP0024TM"})
     setattr(hk.data['hk_dpu_2v5_c'], "meta", {"NIXS": "NIXD0028", "PCF_CURTX": "CIXP0026TM"})
+    raw_to_engineering_product(hk, idbm)
 
-    hk.data
+    print(1)
+
+
+def test_calibration_hk_many(soc_hk, idbm):
+
+    idbm.download_version("2.26.35", force=True)
+    tstart = perf_counter()
+
+    prod_lb = LevelB.from_tm(SOCPacketFile(soc_hk))
+    hk = MaxiReport.from_levelb(prod_lb)
+
+    setattr(hk, "idb", {"2.26.34": (653981065.0, 653991065.0),
+                        "2.26.35": (653991065.0, 654073673.0)})
+    setattr(hk.data['hk_dpu_pcb_t'], "meta", {"NIXS": "NIXD0025", "PCF_CURTX": "CIXP0024TM"})
+    setattr(hk.data['hk_dpu_2v5_c'], "meta", {"NIXS": "NIXD0028", "PCF_CURTX": "CIXP0026TM"})
+    raw_to_engineering_product(hk, idbm)
+
+    tend = perf_counter()
+
+    print('Time taken %f', tend - tstart)
+    print(1)

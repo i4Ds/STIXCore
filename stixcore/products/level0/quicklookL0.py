Bugfix Spectrogram RCR and some QL units (#189)
* Fix RCR reporting incorrectly in BSD Spectrograms

* Fix small bug in primary header generation

* Add some missing units to QL data

stixcore/products/level0/quicklookL0.py
@@ -164,7 +164,7 @@ class LightCurve(QLProduct):
         data['triggers_err'] = np.float32(np.sqrt(triggers_var))
         data['rcr'] = np.hstack(packets.get_value('NIX00276')).flatten().astype(np.ubyte)
         data.add_meta(name='rcr', nix='NIX00276', packets=packets)
-        data['counts'] = counts.T.astype(get_min_uint(counts))
+        data['counts'] = (counts.T*u.ct).astype(get_min_uint(counts))
         data.add_meta(name='counts', nix='NIX00272', packets=packets)
         data['counts_err'] = np.float32(np.sqrt(counts_var).T * u.ct)
 
@@ -233,7 +233,7 @@ class Background(QLProduct):
         data['triggers'] = triggers.astype(get_min_uint(triggers))
         data.add_meta(name='triggers', nix='NIX00274', packets=packets)
         data['triggers_err'] = np.float32(np.sqrt(triggers_var))
-        data['counts'] = counts.T.astype(get_min_uint(counts))
+        data['counts'] = (counts.T*u.ct).astype(get_min_uint(counts))
         data.add_meta(name='counts', nix='NIX00278', packets=packets)
         data['counts_err'] = np.float32(np.sqrt(counts_var).T * u.ct)
 
@@ -622,9 +622,9 @@ class EnergyCalibration(QLProduct):
         data['timedel'] = duration[unique_time_indices]
         data.add_meta(name='timedel', nix='NIX00122', packets=packets)
 
-        data['counts'] = full_counts.astype(get_min_uint(full_counts))
+        data['counts'] = (full_counts*u.ct).astype(get_min_uint(full_counts))
         data.add_meta(name='counts', nix='NIX00158', packets=packets)
-        data['counts_err'] = np.sqrt(full_counts_var).astype(np.float32)
+        data['counts_err'] = (np.sqrt(full_counts_var)*u.ct).astype(np.float32)
         data['control_index'] = np.arange(len(control)).astype(np.uint16)
 
         return cls(service_type=packets.service_type,

stixcore/products/level0/scienceL0.py
@@ -608,14 +608,15 @@ class Spectrogram(ScienceProduct):
         control['detector_mask'] = np.unique(
             fix_detector_mask(control, _get_detector_mask(packets)[0]), axis=0)
         control.add_meta(name='detector_mask', nix='NIX00407', packets=packets)
-        control['rcr'] = np.unique(packets.get_value('NIX00401', attr='value'))[0]
-        control.add_meta(name='rcr', nix='NIX00401', packets=packets)
+        raw_rcr = packets.get_value('NIX00401', attr='value')
 
         e_min = np.array(packets.get_value('NIXD0442'))
         e_max = np.array(packets.get_value('NIXD0443'))
         energy_unit = np.array(packets.get_value('NIXD0019')) + 1
         num_times = np.array(packets.get_value('NIX00089'))
         total_num_times = num_times.sum()
+
+        rcr = np.hstack([np.full(nt, rcr) for rcr, nt in zip(raw_rcr, num_times)]).astype(np.ubyte)
 
         counts = np.array(packets.get_value('NIX00268'))
         counts_var = np.array(packets.get_value('NIX00268', attr='error'))
@@ -694,6 +695,8 @@ class Spectrogram(ScienceProduct):
         data['timedel'] = deltas
         data['timedel'].meta = {'NIXS': ['NIX00441', 'NIX00269']}
         data.add_basic(name='triggers', nix='NIX00267', packets=packets)
+        data['rcr'] = rcr
+        data.add_meta(name='rcr', nix='NIX00401', packets=packets)
         data['triggers'].dtype = get_min_uint(data['triggers'])
         data.add_basic(name='triggers_err', nix='NIX00267', attr='error', packets=packets)
         data['triggers_err'] = np.float32(data['triggers_err'])
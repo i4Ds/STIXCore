7bb3ce072b1b006a86cdca09431db15a61fb8ca2
Nicky Hochmuth <nicky.thecrag@gmail.com>
2022-02-17 09:25:08 +0100

    first aspect processing version running

--- stixcore/io/fits/processors.py
+++ stixcore/io/fits/processors.py
@@ -1,6 +1,5 @@
 """Module for the different processing levels."""
 import logging
-from pathlib import Path
 from datetime import datetime
 
 import numpy as np
@@ -11,7 +10,6 @@ from astropy.io.fits import table_to_hdu
 from astropy.table import QTable
 
 import stixcore
-from stixcore.config.config import CONFIG
 from stixcore.ephemeris.manager import Spice
 from stixcore.products.level0.scienceL0 import Aspect
 from stixcore.products.product import Product
@@ -508,9 +506,9 @@ class FitsL1Processor(FitsL0Processor):
             ('XPOSURE', exposure, '[s] Total effective exposure time')
         )
 
-        soop_keywords = self.soop_manager.get_keywords(start=product.utc_timerange.start,
-                                                       end=product.utc_timerange.end,
-                                                       otype=SoopObservationType.ALL)
+        soop_keywords = SOOPManager.instance.get_keywords(start=product.utc_timerange.start,
+                                                          end=product.utc_timerange.end,
+                                                          otype=SoopObservationType.ALL)
         soop_headers = tuple(kw.tuple for kw in soop_keywords)
         soop_defaults = (
             ('OBS_MODE', '', 'Observation mode'),
@@ -544,7 +542,7 @@ class FitsL1Processor(FitsL0Processor):
             Spice.instance.get_fits_headers(start_time=product.utc_timerange.start,
                                             average_time=product.utc_timerange.center)
 
-        return headers + data_headers + soop_headers + time_headers + ephemeris_headers
+        return headers + data_headers + soop_headers + time_headers, ephemeris_headers
 
     def write_fits(self, product):
         """
@@ -590,9 +588,10 @@ class FitsL1Processor(FitsL0Processor):
                                   for version, range in product.idb_versions.items()],
                                   names=["version", "obt_start", "obt_end"])
 
-            primary_header = self.generate_primary_header(filename, prod)
+            primary_header, header_override = self.generate_primary_header(filename, prod)
             primary_hdu = fits.PrimaryHDU()
             primary_hdu.header.update(primary_header)
+            primary_hdu.header.update(header_override)
             primary_hdu.header.update({'HISTORY': 'Processed by STIX'})
 
             # Convert time to be relative to start date
@@ -649,8 +648,11 @@ class FitsL2Processor(FitsL1Processor):
         # if product.level != 'L2':
         #    raise ValueError(f"Try to crate FITS file L2 for {product.level} data product")
 
-        L1headers = super().generate_primary_header(filename, product) if product.fits_header \
-            is None else product.fits_header.items()
+        if product.fits_header is None:
+            L1, o = super().generate_primary_header(filename, product)
+            L1headers = L1 + o
+        else:
+            L1headers = product.fits_header.items()
 
         # new or override keywords
         L2headers = (
@@ -661,97 +663,3 @@ class FitsL2Processor(FitsL1Processor):
         )
 
         return L1headers, L2headers
-
-    def write_fits(self, product):
-        """
-        Write level 0 products into fits files.
-
-        Parameters
-        ----------
-        product : `stixcore.product.level0`
-
-        Returns
-        -------
-        list
-            of created file as `pathlib.Path`
-
-        """
-        created_files = []
-        if callable(getattr(product, 'to_days', None)):
-            products = product.to_days()
-        else:
-            products = product.to_requests()
-
-        for prod in products:
-            filename = self.generate_filename(product=prod, version=1)
-            # start_day = np.floor((prod.obs_beg.as_float()
-            #                       // (1 * u.day).to('s')).value * SEC_IN_DAY).astype(int)
-
-            parts = [prod.level, prod.utc_timerange.center.strftime('%Y/%m/%d'), prod.type.upper()]
-            path = self.archive_path.joinpath(*[str(x) for x in parts])
-            path.mkdir(parents=True, exist_ok=True)
-
-            fitspath = path / filename
-            if fitspath.exists():
-                logger.info('Fits file %s exists appending data', fitspath.name)
-                existing = Product(fitspath)
-                logger.debug('Existing %s, Current %s', existing, prod)
-                prod = prod + existing
-                logger.debug('Combined %s', prod)
-
-            control = prod.control
-            data = prod.data
-
-            idb_versions = QTable(rows=[(version, range.start.as_float(), range.end.as_float())
-                                  for version, range in product.idb_versions.items()],
-                                  names=["version", "obt_start", "obt_end"])
-
-            l1_header, l2_header = self.generate_primary_header(filename, prod)
-            primary_hdu = fits.PrimaryHDU()
-            primary_hdu.header.update(l1_header)
-            primary_hdu.header.update(l2_header)
-
-            # Convert time to be relative to start date
-            # it is important that the change to the relative time is done after the header is
-            # generated as this will use the original SCET time data
-            data['time'] = (data['time'] - prod.scet_timerange.start).as_float()
-            data['timedel'] = data['timedel'].as_float()
-            try:
-                control['time_stamp'] = control['time_stamp'].as_float()
-            except KeyError as e:
-                if 'time_stamp' not in repr(e):
-                    raise e
-
-            control_enc = fits.connect._encode_mixins(control)
-            control_hdu = table_to_hdu(control_enc)
-            control_hdu.name = 'CONTROL'
-
-            data_enc = fits.connect._encode_mixins(data)
-            data_hdu = table_to_hdu(data_enc)
-            data_hdu.name = 'DATA'
-
-            idb_enc = fits.connect._encode_mixins(idb_versions)
-            idb_hdu = table_to_hdu(idb_enc)
-            idb_hdu.name = 'IDB_VERSIONS'
-
-            hdus = [primary_hdu, control_hdu, data_hdu, idb_hdu]
-
-            if getattr(prod, 'get_energies', False) is not False:
-                elow, ehigh = prod.get_energies()
-                energies = QTable()
-                energies['channel'] = range(len(elow))
-                energies['e_low'] = elow * u.keV
-                energies['e_high'] = ehigh * u.keV
-
-                energy_enc = fits.connect._encode_mixins(energies)
-                energy_hdu = table_to_hdu(energy_enc)
-                energy_hdu.name = 'ENERGIES'
-                hdus.append(energy_hdu)
-
-            hdul = fits.HDUList(hdus)
-
-            filetowrite = path / filename
-            logger.debug(f'Writing fits file to {filetowrite}')
-            hdul.writeto(filetowrite, overwrite=True, checksum=True)
-            created_files.append(filetowrite)
-        return created_files

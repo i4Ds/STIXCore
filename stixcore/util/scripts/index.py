2664aefd247c33df422d63a35311dbc418d71d3c
Nicky Hochmuth <nicky.thecrag@gmail.com>
2022-03-03 17:04:42 +0100

    fix update script

--- stixcore/util/scripts/index.py
+++ stixcore/util/scripts/index.py
@@ -1,46 +1,49 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
 import os
 from pathlib import Path
 
 import git
 
-print("Content-Type: text/html;charset=utf-8", flush=True)
-print("Access-Control-Allow-Origin: *")
-print("Pragma: no-cache", flush=True)
-print("Expires: 0", flush=True)
-print(flush=True)
+if __name__ == '__main__':
 
-dir = Path("/data/stix/end2end/STIXCore")
+    print("Content-Type: text/html;charset=utf-8", flush=True)
+    print("Access-Control-Allow-Origin: *")
+    print("Pragma: no-cache", flush=True)
+    print("Expires: 0", flush=True)
+    print(flush=True)
 
-os.chdir(dir)
-g = git.cmd.Git(dir)
+    dir = Path("/data/stix/end2end/STIXCore")
 
-p_result = g.pull()
+    os.chdir(dir)
+    g = git.cmd.Git(dir)
 
-repo = git.Repo(dir)
-commit = repo.head.commit
+    p_result = g.pull()
 
-# tagmap = {}
-# for t in repo.tags:
-#   tagmap.setdefault(repo.commit(t), []).append(t)
-#
-# curtag = tagmap[commit]
+    repo = git.Repo(dir)
+    commit = repo.head.commit
 
-tag = "head"
+    # tagmap = {}
+    # for t in repo.tags:
+    #   tagmap.setdefault(repo.commit(t), []).append(t)
+    #
+    # curtag = tagmap[commit]
 
-print(f"Update Project: {p_result}")
+    tag = "head"
 
-print("delete old files")
-delout = os.popen("rm -rv ./stixcore/data/test/products/end2end/").read()
-print(delout)
+    print(f"Update Project: {p_result}")
 
-print("Recreate the test files")
-runout = os.popen("./venv/bin/python ./stixcore/util/scripts/end2end_testing.py").read()
-print(runout)
+    print("delete old files")
+    delout = os.popen("rm -rv ./stixcore/data/test/products/end2end/").read()
+    print(delout)
 
-print("pack and copy test files")
-copyout = os.popen(f"zip -FSrj ../data/{tag}.zip ./stixcore/data/test/products/end2end/").read()
-print(copyout)
+    print("Recreate the test files")
+    runout = os.popen("./venv/bin/python ./stixcore/util/scripts/end2end_testing.py").read()
+    print(runout)
 
-print('All end2end rebuilds done!')
+    print("pack and copy test files")
+    copyout = os.popen(f"zip -FSrj ../data/{tag}.zip ./stixcore/data/test/products/end2end/").read()
+    print(copyout)
+
+    print('All end2end rebuilds done!')

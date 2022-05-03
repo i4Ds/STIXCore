a4dab3970add3d7e2d024b90ce54fca226e2fd00
Shane Maloney <shane.maloney@tcd.ie>
2022-05-03 17:40:25 +0100

    Skip tests for until test data sorted

--- stixcore/processing/tests/test_ll01.py
+++ stixcore/processing/tests/test_ll01.py
@@ -1,9 +1,12 @@
 from pathlib import Path
 from unittest.mock import patch
 
+import pytest
+
 from stixcore.processing.TMtoLL01 import main, process_request
 
 
+@pytest.mark.skip(reason="Need to figure out test data")
 def test_process_request(monkeypatch, tmp_path):
     monkeypatch.setenv("instr_input_requests", "/Users/shane/Projects/STIX/LLDP/top_input/STIX")
     monkeypatch.setenv("instr_output", "/Users/shane/Projects/STIX/LLDP/instr_output")
@@ -13,6 +16,7 @@ def test_process_request(monkeypatch, tmp_path):
     process_request(request, out)
 
 
+@pytest.mark.skip(reason="Need to figure how to mock enviroment variables")
 @patch('stixcore.processing.TMtoLL01.get_paths')
 def test_main(mock_paths):
     outpath = Path('/Users/shane/Projects/STIX/LLDP/instr_output')

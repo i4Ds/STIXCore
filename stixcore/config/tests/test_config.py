3057889d0c449195cf89c0d2c6d41ce1d2366345
Nicky Hochmuth <nicky.thecrag@gmail.com>
2022-05-10 17:57:02 +0200

    cleanup

--- stixcore/config/tests/test_config.py
+++ stixcore/config/tests/test_config.py
@@ -4,6 +4,3 @@ from stixcore.config.config import CONFIG
 def test_parse():
     assert CONFIG.get("Paths", "tm_archive", fallback="test") == ''
     assert CONFIG.get("Paths", "tm_archive2", fallback="test") == 'test'
-
-    assert not CONFIG.getboolean("IDLBridge", "enabled", fallback=False)
-    assert CONFIG.getboolean("IDLBridge", "enabled2", fallback=True)

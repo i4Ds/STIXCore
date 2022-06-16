Fixed grid transmission calculation

stixcore/calibration/grid.py
@@ -46,23 +46,24 @@ def get_grid_internal_shadowing(xy_flare):
 
     # Paolo Massa (September 2021): corrected the definition of 'o_flare'
 
-    #  if (xy_flare_stix[0] >= 0) AND (xy_flare_stix[1] >= 0)
-    #  then o_flare=acos(xy_flare_stix[1]/r_flare)/!pi*180.
-    #  if (xy_flare_stix[0] >= 0) AND (xy_flare_stix[1] <= 0)
-    #  then o_flare=90+asin(abs(xy_flare_stix[1])/r_flare)/!pi*180.
-    #  if (xy_flare_stix[0] <= 0) AND (xy_flare_stix[1] <= 0)
-    #  then o_flare=acos(abs(xy_flare_stix[1])/r_flare)/!pi*180.
-    #  if (xy_flare_stix[0] <= 0) AND (xy_flare_stix[1] >= 0)
-    #  then o_flare=90+asin(xy_flare_stix[1]/r_flare)/!pi*180.
-
     if (xy_flare_stix[0] >= 0) and (xy_flare_stix[1] >= 0):
-        o_flare = np.arccos(xy_flare_stix[1] / r_flare)
+        o_flare=np.arccos(xy_flare_stix[1]/r_flare)
     if (xy_flare_stix[0] >= 0) and (xy_flare_stix[1] <= 0):
-        o_flare = np.pi*u.rad/2 - np.arcsin(abs(xy_flare_stix[1]) / r_flare)
+        o_flare=90*u.deg+np.arcsin(np.abs(xy_flare_stix[1])/r_flare)
     if (xy_flare_stix[0] <= 0) and (xy_flare_stix[1] <= 0):
-        o_flare = -np.pi*u.rad/2 + np.arcsin(xy_flare_stix[1] / r_flare)
+        o_flare=np.arccos(np.abs(xy_flare_stix[1])/r_flare)
     if (xy_flare_stix[0] <= 0) and (xy_flare_stix[1] >= 0):
-        o_flare = -np.arccos(xy_flare_stix[1] / r_flare)
+        o_flare = 90*u.deg + np.arcsin(xy_flare_stix[1]/r_flare)
+
+    # if (xy_flare_stix[0] >= 0) and (xy_flare_stix[1] >= 0):
+    #     o_flare = np.arccos(xy_flare_stix[1] / r_flare)
+    # if (xy_flare_stix[0] >= 0) and (xy_flare_stix[1] <= 0):
+    #     o_flare = np.pi*u.rad/2 - np.arcsin(abs(xy_flare_stix[1]) / r_flare)
+    # if (xy_flare_stix[0] <= 0) and (xy_flare_stix[1] <= 0):
+    #     o_flare = -np.pi*u.rad/2 + np.arcsin(xy_flare_stix[1] / r_flare)
+    # if (xy_flare_stix[0] <= 0) and (xy_flare_stix[1] >= 0):
+    #     o_flare = -np.arccos(xy_flare_stix[1] / r_flare)
+
 
     # Paolo Massa (September 2021): corrected the definition of 'rel_f' and 'rel_r'. Indeed, the
     # grids in this case are considered looking towards the Sun. A minus sign is added to
@@ -72,15 +73,15 @@ def get_grid_internal_shadowing(xy_flare):
     #  #orientations of grids relative to flare
     #  rel_f=abs(front['o']-o_flare)
     #  rel_r=abs(rear['o']-o_flare)
-    rel_f = np.abs(-front['o']*u.deg - o_flare)
-    rel_r = np.abs(-rear['o']*u.deg - o_flare)
+    rel_f = np.abs((180 - front['o'])*u.deg - o_flare)
+    rel_r = np.abs((180 - rear['o'])*u.deg - o_flare)
 
     # correction only applies to component normal to the grid orientation
     cor_f = np.sin(rel_f)
     cor_r = np.sin(rel_r)
 
     # nominal thickness with glue
-    nom_h = 0.42
+    nom_h = 0.42 * u.mm
     # maximal internal grid shadowing when see for grids with orientation of 90 degrees from
     # center to flare line
     max_c = np.tan(r_flare)*nom_h
@@ -91,11 +92,11 @@ def get_grid_internal_shadowing(xy_flare):
 
     # for bridges correction is largest for grids with orientation parallel to the center-flare
     # line
-    np.cos(rel_f)
-    np.cos(rel_r)
+    b_cor_f = np.cos(rel_f)
+    b_cor_f = np.cos(rel_r)
     # the shadow to be added to bridge width (see below)
-    cor_r * max_c
-    cor_f * max_c
+    b_shadow_r = cor_r * max_c
+    b_shadow_f = cor_f * max_c
 
     gtrans32f = np.zeros(32) - 2
     gtrans32r = np.zeros(32) - 2
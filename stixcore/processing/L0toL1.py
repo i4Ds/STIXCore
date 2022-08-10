10af36b81276badb8967fbdc8c67bdf27594b881
Nicky Hochmuth <nicky.thecrag@gmail.com>
2022-08-10 16:06:10 +0200

    change parallel processing to batch by SCI product

--- stixcore/processing/L0toL1.py
+++ stixcore/processing/L0toL1.py
@@ -18,6 +18,15 @@ logger = get_logger(__name__)
 
 
 class Level1:
+    """Processing step from L0 to L1.
+
+    Input are L0 Fits files and the result is written to FITS as well. Daily products will be added
+    to existing FITS files if already present.
+
+    Groups all filles into parallel processes by product type if daily products. For by request
+    products it is also grouped by type but additional into batches of a given size.
+    A priority heuristic starts with ql data as this takes longest.
+    """
     def __init__(self, source_dir, output_dir):
         self.source_dir = Path(source_dir)
         self.output_dir = Path(output_dir)
@@ -28,15 +37,31 @@ class Level1:
         all_files = list()
         if files is None:
             files = self.level0_files
-
         product_types = defaultdict(list)
+        product_types_batch = defaultdict(int)
+        batch_size = CONFIG.getint('Pipeline', 'parallel_batchsize_L1', fallback=150)
+
         for file in files:
-            # group by service,subservice,ssid example: 'L0/21/6/30'
-            product_types[str(file.parent)].append(file)
+            # group by service,subservice, ssid example: 'L0/21/6/30' as default
+            # or (prio, service, subservice, [SSID], [BATCH]) if all data is available
+            batch = 0
+            prio = 3
+            product_type = str(file.parent)
+            if 'L0' in file._parts:
+                product_type = tuple(map(int, file._parts[file._parts.index('L0')+1:-1]))
+                if (product_type[0] == 21 and
+                        product_type[-1] in {20, 21, 22, 23, 24, 42}):  # sci data
+                    product_types_batch[product_type] += 1
+                    prio = 2
+                elif product_type[0] == 21:  # ql data
+                    prio = 1
+                batch = product_types_batch[product_type] // batch_size
+            product_types[(prio, ) + product_type + (batch, )].append(file)
 
         jobs = []
         with ProcessPoolExecutor() as executor:
-            for pt, files in product_types.items():
+            # simple heuristic that the daily QL data takes longest so we start early
+            for pt, files in sorted(product_types.items()):
                 jobs.append(executor.submit(process_type, files,
                                             processor=FitsL1Processor(self.output_dir),
                                             soopmanager=SOOPManager.instance,
@@ -61,6 +86,7 @@ def process_type(files, *, processor, soopmanager, spice_kernel_path, config):
 
     for file in files:
         l0 = Product(file)
+        logger.info(f"processing file: {file}")
         try:
             tmp = Product._check_registered_widget(level='L1', service_type=l0.service_type,
                                                    service_subtype=l0.service_subtype,

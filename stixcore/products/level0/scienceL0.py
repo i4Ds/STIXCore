2e4afd06e34e124ccfa2fb7bf2708dd2c88ed7fe
Nicky Hochmuth <nicky.thecrag@gmail.com>
2022-08-11 22:35:07 +0200

    fixes for set data type

--- stixcore/products/level0/scienceL0.py
+++ stixcore/products/level0/scienceL0.py
@@ -202,7 +202,7 @@ class RawPixelData(ScienceProduct):
         data.add_data('pixel_masks', _get_pixel_mask(packets, 'NIXD0407'))
         data.add_data('detector_masks', _get_detector_mask(packets))
         triggers = np.array([packets.get_value(f'NIX00{i}') for i in range(408, 424)]).T
-        data['triggers'] = triggers.T.astype(get_min_uint(triggers))
+        data['triggers'] = triggers.astype(get_min_uint(triggers))
         data['triggers'].meta = {'NIXS': [f'NIX00{i}' for i in range(408, 424)]}
         data.add_basic(name='num_samples', nix='NIX00406', packets=packets, dtype=np.uint16)
 

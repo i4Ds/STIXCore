3e69f90142e5c927bcdffb5f9b6e8750713e72ce
Nicky Hochmuth <nicky.thecrag@gmail.com>
2022-08-16 17:47:43 +0200

    update idb references (#267)
    * update idb references
    
    * fix test for latest idb .36

--- stixcore/idb/tests/test_manager.py
+++ stixcore/idb/tests/test_manager.py
@@ -81,7 +81,7 @@ def test_find_version(idb_manager):
 
     # fall back to the default
     idb = idb_manager.get_idb(obt=SCETime(coarse=2 ** 31 - 1, fine=0))
-    assert idb.get_idb_version() == "2.26.35"
+    assert idb.get_idb_version() == "2.26.36"
 
     assert idb_manager.find_version(obt=None) == "2.26.31"
 
@@ -90,8 +90,8 @@ def test_find_version(idb_manager):
 def test_get_versions(idb_manager):
     versions = idb_manager.get_versions()
     assert isinstance(versions, list)
-    # zjust 5 not 6 as 2.26.2 contains no file
-    assert len(versions) == 7
+    # just 5 not 6 as 2.26.2 contains no file
+    assert len(versions) == 8
 
 
 @pytest.mark.remote_data

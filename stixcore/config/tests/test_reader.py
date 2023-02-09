e8385eeda2004dd1bef8d8615257d9f38034ff8a
Shane Maloney <shane.maloney@tcd.ie>
2023-02-09 12:34:23 +0000

    Add test and update data submodule

--- stixcore/config/tests/test_reader.py
+++ stixcore/config/tests/test_reader.py
@@ -21,9 +21,17 @@ def test_read_sci_energy_channels():
 
 
 def test_get_sci_channels():
-    sci_channel = get_sci_channels(datetime.now())
-    assert isinstance(sci_channel, QTable)
-    assert len(sci_channel) == 33
+    sci_channels = get_sci_channels(datetime.now())
+    assert isinstance(sci_channels, QTable)
+    assert len(sci_channels) == 33
+    assert_equal(sci_channels['Elower'][1], 4.0 * u.keV)
+    assert_equal(sci_channels['Eupper'][1], 5.0 * u.keV)
+
+    sci_channels = get_sci_channels(datetime(2023, 1, 30, 12))
+    assert isinstance(sci_channels, QTable)
+    assert len(sci_channels) == 33
+    assert_equal(sci_channels['Elower'][1], 4.0 * u.keV)
+    assert_equal(sci_channels['Eupper'][1], 4.45 * u.keV)
 
     with pytest.raises(ValueError, match=r"No Science Energy.*"):
         get_sci_channels(datetime(2018, 1, 1))

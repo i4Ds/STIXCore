915275c66b8c83b4507b55cce532bb16540eb061
Nicky Hochmuth <nicky.thecrag@gmail.com>
2023-02-15 11:05:27 +0100

    add soop api as input for all instruments soops (#315)
    * add soop api as input for all instruments soops
    
    * add ref to requests
    
    * fix time shift correction for rpd
    
    * update ddpd script
    
    * minor format changes

--- stixcore/processing/pipeline_cli.py
+++ stixcore/processing/pipeline_cli.py
@@ -173,7 +173,6 @@ def main():
         spicemeta = _spm.get_latest_mk(top_n=30)
 
     Spice.instance = Spice(spicemeta)
-    print(f"Spice kernel @: {Spice.instance.meta_kernel_path}")
 
     soc = SOCManager(Path(CONFIG.get('Paths', 'tm_archive')))
 

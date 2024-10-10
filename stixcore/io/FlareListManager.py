1c7c25de033fcb39860274b46aa476b0c1615569
Nicky Hochmuth <nicky.thecrag@gmail.com>
2024-10-10 10:23:26 +0200

    daily processing task

--- stixcore/io/FlareListManager.py
+++ stixcore/io/FlareListManager.py
@@ -1,109 +1,67 @@
 import sys
 import time
-import tempfile
-import urllib.request
-from datetime import date, datetime, timedelta
+from pathlib import Path
+from datetime import datetime, timedelta
 
-import numpy as np
+import pandas as pd
+from stixdcpy.net import Request as stixdcpy_req
 
-from astropy.io import ascii
-from astropy.table import Table
-from astropy.table.operations import unique, vstack
-
-from stixcore.config.config import CONFIG
 from stixcore.util.logging import get_logger
 from stixcore.util.singleton import Singleton
 
-__all__ = ['FlareListManager']
+__all__ = ['FlareListManager', 'SDCFlareListManager']
 
 logger = get_logger(__name__)
 
 
-class FlareListManager(metaclass=Singleton):
-    """Manages metadata for BSD requests
+class FlareListManager():
+    @property
+    def flarelist(self):
+        return self._flarelist
 
-    The rid is used for a lookup in a csv table file where additional data
-    connected to a BSD request is stored. Such as a description of the request
-    purpose or state dependent configurations that are not part of the TM data.
-    Most important the trigger scaling factor that was used if the trigger scaling
-    schema is active.
+    @flarelist.setter
+    def flarelist(self, value):
+        self._flarelist = value
 
-    The data of th LUT is required over the the API endpoint:
-    https://datacenter.stix.i4ds.net/api/bsd/info/
+    @property
+    def flarelistname(self):
+        return type(self).__name__
+
+
+class SDCFlareListManager(FlareListManager, metaclass=Singleton):
+    """Manages a local copy of the operational flarelist provided by stix data datacenter
+
+    TODO
     """
 
     def __init__(self, file, update=False):
-        """Creates the manager by pointing to the LUT files and setting the update strategy.
+        """Creates the manager by pointing to the flarelist file (csv) and setting the update strategy.
 
         Parameters
         ----------
         file : Path
-            points to the LUT file
+            points to the csv file
         update : bool, optional
-            Update strategy: is the LUT file updated via API?, by default False
+            Update strategy: is the flarelist file updated via API?, by default False
         """
         self.file = file
         self.update = update
-        self.rid_lut = FlareListManager.read_rid_lut(self.file, self.update)
+        self._flarelist = SDCFlareListManager.read_flarelist(self.file, self.update)
 
     def __str__(self) -> str:
-        return f"file: {self.file} update: {self.update} size: {len(self.rid_lut)}"
+        return (f"{self.flarelistname}: file: {self.file} update: "
+                f"{self.update} size: {len(self.flarelist)}")
 
-    def update_lut(self):
-        """Updates the LUT file via api request.
+    def update_list(self):
+        """Updates the flarelist file via api request.
 
         Will create a new file if not available or do a incremental update otherwise,
         using the last entry time stamp.
         """
-        self.rid_lut = FlareListManager.read_rid_lut(self.file, update=self.update)
-
-    def get_reason(self, rid):
-        """Gets the verbal description of the request purpose by combining several descriptive columns.
-
-        Parameters
-        ----------
-        rid : int
-            the BSD request id
-
-        Returns
-        -------
-        str
-            verbal description of the request purpose
-        """
-        request = self.rid_lut.loc[rid]
-        reason = " ".join(np.atleast_1d(request['description']))
-        return reason
-
-    def get_scaling_factor(self, rid):
-        """Gets the trigger descaling factor connected to the BSD request.
-
-        Parameters
-        ----------
-        rid : int
-            the BSD request id
-
-        Returns
-        -------
-        int
-            the proposed trigger descaling factor to use for the BSD processing
-
-        Raises
-        ------
-        ValueError
-            if no or to many entries found for the given rid
-        """
-        try:
-            request = self.rid_lut.loc[rid]
-        except KeyError:
-            raise ValueError("can't get scaling factor: no request founds for rid: {rid}")
-        scaling_factor = np.atleast_1d(request['scaling_factor'])
-        if len(scaling_factor) > 1:
-            raise ValueError("can't get scaling factor: to many request founds for rid: {rid}")
-        scf = scaling_factor[0].strip()
-        return 30 if scf == '' else int(float(scf))
+        self.flarelist = SDCFlareListManager.read_flarelist(self.file, update=self.update)
 
     @classmethod
-    def read_rid_lut(cls, file, update=False):
+    def read_flarelist(cls, file, update=False):
         """Reads or creates the LUT of all BSD RIDs and the request reason comment.
 
         On creation or update an api endpoint from the STIX data center is used
@@ -121,67 +79,61 @@ class FlareListManager(metaclass=Singleton):
         Table
             the LUT od RIDs and request reasons.
         """
-        converters = {'_id': np.uint, 'unique_id': np.uint, 'start_utc': datetime,
-                      'duration': np.uint, 'type': str, 'subject': str,
-                      'purpose': str, 'scaling_factor': str, 'ior_id':  str, 'comment': str}
-
         if update or not file.exists():
-            rid_lut = Table(names=converters.keys(), dtype=converters.values())
             # the api is limited to batch sizes of a month. in order to get the full table we have
             # to ready each month after the start of STIX
-            last_date = date(2019, 1, 1)
-            today = date.today()
+            last_date = datetime(2020, 1, 1, 0, 0, 0)
+            today = datetime.now()  # - timedelta(days=60)
+            flare_df_lists = []
             if file.exists():
-                rid_lut = ascii.read(file, delimiter=",", converters=converters,
-                                     guess=False, quotechar='"')
-                mds = rid_lut['start_utc'].max()
+                old_list = pd.read_csv(file)
+                mds = old_list['start_UTC'].max()
                 try:
-                    last_date = datetime.strptime(mds, '%Y-%m-%dT%H:%M:%S').date()
+                    last_date = datetime.strptime(mds, '%Y-%m-%dT%H:%M:%S')
                 except ValueError:
-                    last_date = datetime.strptime(mds, '%Y-%m-%dT%H:%M:%S.%f').date()
-
+                    last_date = datetime.strptime(mds, '%Y-%m-%dT%H:%M:%S.%f')
+                flare_df_lists = [old_list]
             if not file.parent.exists():
-                logger.info(f'path not found to rid lut file dir: {file.parent} creating dir')
+                logger.info(f'path not found to flare list file dir: {file.parent} creating dir')
                 file.parent.mkdir(parents=True, exist_ok=True)
-            rid_lut_file_update_url = CONFIG.get('Publish', 'rid_lut_file_update_url')
 
             try:
                 while (last_date < today):
                     last_date_1m = last_date + timedelta(days=30)
-                    ldf = last_date.strftime('%Y%m%d')
-                    ld1mf = last_date_1m.strftime('%Y%m%d')
-                    update_url = f"{rid_lut_file_update_url}{ldf}/{ld1mf}"
-                    logger.info(f'download publish lut file: {update_url}')
+                    logger.info(f'download flare list chunk: {last_date.isoformat()}'
+                                f'/{last_date_1m.isoformat()}')
+                    flares = stixdcpy_req.fetch_flare_list(last_date.isoformat(),
+                                                           last_date_1m.isoformat())
                     last_date = last_date_1m
-                    updatefile = tempfile.NamedTemporaryFile().name
-                    urllib.request.urlretrieve(update_url, updatefile)
-                    update_lut = ascii.read(updatefile, delimiter=",", converters=converters,
-                                            guess=False, quotechar='"')
-
-                    if len(update_lut) < 1:
-                        continue
-                    logger.info(f'found {len(update_lut)} entries')
-                    rid_lut = vstack([rid_lut, update_lut])
+                    if len(flares) > 0:
+                        flare_df_lists.append(pd.DataFrame(flares))
+                        logger.info(f'found {len(flares)} flares')
                     # the stix datacenter API is throttled to 2 calls per second
                     time.sleep(0.5)
             except Exception:
-                logger.error("RID API ERROR", exc_info=True)
+                logger.error("FLARELIST API ERROR", exc_info=True)
 
-            rid_lut = unique(rid_lut, silent=True)
-            ascii.write(rid_lut, file, overwrite=True, delimiter=",", quotechar='"')
-            logger.info(f'write total {len(rid_lut)} entries to local storage')
+            full_flare_list = pd.concat(flare_df_lists)
+
+            full_flare_list.drop_duplicates(inplace=True)
+            full_flare_list.sort_values(by="peak_UTC", inplace=True)
+            full_flare_list.reset_index(inplace=True, drop=True)
+            logger.info(f'write total {len(full_flare_list)} flares to local storage')
+            full_flare_list.to_csv(file, index_label=False)
         else:
-            logger.info(f"read rid-lut from {file}")
-            rid_lut = ascii.read(file, delimiter=",", converters=converters)
+            logger.info(f"read flare list from {file}")
+            full_flare_list = pd.read_csv(file)
 
-        rid_lut['description'] = [", ".join(r.values()) for r in
-                                  rid_lut['subject', 'purpose', 'comment'].filled()]
-        rid_lut.add_index('unique_id')
-
-        return rid_lut
+        return full_flare_list
 
 
 if 'pytest' in sys.modules:
     # only set the global in test scenario
     from stixcore.data.test import test_data
-    FlareListManager.instance = FlareListManager(test_data.rid_lut.RID_LUT, update=False)
+    SDCFlareListManager.instance = SDCFlareListManager(test_data.rid_lut.RID_LUT, update=False)
+
+
+if __name__ == '__main__':
+    flm = SDCFlareListManager(Path("./flarelist.scv"), update=False)
+    print(flm)
+    print(flm.flarelist)
